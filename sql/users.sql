8529e8f0cf3d6119ffce32d67e5de596a974e207
komnac <kirillkomlev@gmail.com>
2015-06-21 13:42:28 +0300

    Rework users tables structure

--- sql/users.sql
+++ sql/users.sql
@@ -3,8 +3,10 @@ DROP TABLE IF EXISTS `tm_users`;
 CREATE TABLE `tm_users` (
   `login` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT '',
-  `name` varchar(255) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `name` varchar(255) DEFAULT '',
+  `password` varchar(100) DEFAULT '',
   `is_admin` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO tm_users VALUES ('admin', '', 'Admin', MD5(MD5('123')), 1);
